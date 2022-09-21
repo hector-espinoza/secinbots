ff9c51abb6c607bbfd9ee9fa48cd535cbd02d2cc
hector.espinoza <hectorjonathanespinoza@gmail.com>
2022-09-20 18:16:06 -0700

    Regular update

--- secinbots/urls.py
+++ secinbots/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import os
+import dotenv
 from django.contrib import admin
 from django.urls import path, include
 
+dotenv.load_dotenv(
+    os.path.join(os.path.dirname(__file__), '.env')
+)
+
+#print(dotenv.dotenv_values('SECRET_ADMIN_URL'))
+
 urlpatterns = [
-    path(str(os.getenv('SECRET_ADMIN_URL')) + '/admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('messagex.urls')),
 ]
